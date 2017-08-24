a99c1ad3f29e56c47590f7f0dac5e888daf0e297
Jake Hall <jakeh@xmos.com>
2017-08-24 11:17:06 +0100

    Added types specifiers to XType for struct, union and interface.

--- XType.hs
+++ XType.hs
@@ -4,9 +4,12 @@ import XLexer
 import XUtils
 import Text.Parsec
 
+type Identifier = String
+
 data XTypeSpecifier    =  Void | Char | UChar | Short | UShort | Int | UInt |
                           Chan | ChanEnd | Port | Timer | Clock |
-                          Struct [(XType, String)] | Union [(XType, String)]
+                          Struct Identifier | Union Identifier |
+                          Interface Identifier
 data XTypeQualifier    =  Const | Volatile | In | Out | Buffered
 data XTypeStorageClass =  Auto | Register | Static | Extern | Inline
 data XType = XType [XTypeStorageClass] [XTypeQualifier] XTypeSpecifier
